ce9bfa168211b0dac22f1a4619a01083de73032d
Zhongwen Lian <lianzhongwen@gmail.com>
2023-08-07 14:19:34 +0800

    Update product table schema

--- src/main/resources/db/migration/V202308070807_01__Create_product_table.sql
+++ src/main/resources/db/migration/V202308070807_01__Create_product_table.sql
@@ -1,6 +1,14 @@
-CREATE TABLE PRODUCT(
-    id VARCHAR(50) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price DECIMAL(10,2),
-    status VARCHAR(10) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `product`
+(
+    `id`          VARCHAR(50)  NOT NULL COMMENT '主键',
+    `name`        VARCHAR(255) NOT NULL,
+    `price`       DECIMAL(10, 2),
+    `status`      VARCHAR(10)  NOT NULL,
+    `create_time` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER
+      SET
+      = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;;
