30f93ae8e15acd7e05a8c77c7b30a6c8e4d1a24f
Zhongwen Lian <lianzhongwen@gmail.com>
2023-08-07 15:57:31 +0800

    Add integration test

--- src/integrationTest/java/com/example/BaseIntegrationTest.java
+++ src/integrationTest/java/com/example/BaseIntegrationTest.java
@@ -1,12 +1,16 @@
 package com.example;
 
+import com.github.database.rider.junit5.api.DBRider;
 import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@DBRider
+@ActiveProfiles("api-test")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseIntegrationTest {
