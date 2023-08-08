4a6075ef49699641fd4a7306361b98bf6e225e91
shuang.gao <shuang.gao@thoughtworks.com>
2023-08-08 14:09:22 +0800

    Add order and order product table

---
+++ src/main/resources/db/migration/V202308080808_02__Create_order_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `order` (
+    `id` VARCHAR(32) NOT NULL COMMENT '主键',
+    `customer_id` VARCHAR(32) NOT NULL COMMENT '客户ID',
+    `totalPrice` DECIMAL(10, 2),
+    `status` VARCHAR(32) NOT NULL,
+    `create_time` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER
+      SET
+      = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
