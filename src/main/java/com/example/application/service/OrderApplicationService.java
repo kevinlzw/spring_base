07c24032c87be51245108c2ced0286d9c4efd00c
Shuang Gao <18629659620@163.com>
2023-08-08 23:26:49 +0800

    Add api to get order list

--- src/main/java/com/example/application/service/OrderApplicationService.java
+++ src/main/java/com/example/application/service/OrderApplicationService.java
@@ -1,13 +1,70 @@
 package com.example.application.service;
 
-import com.example.infrastructure.persistence.assembler.OrderDataMapper;
-import com.example.infrastructure.persistence.repository.domain.OrderDomainRepository;
+import com.example.domain.entity.Order;
+import com.example.domain.entity.Product;
+import com.example.domain.repository.OrderRepository;
+import com.example.domain.repository.ProductRepository;
+import com.example.presentation.vo.OrderDto;
+import com.example.presentation.vo.ProductDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class OrderApplicationService {
 
-    private final OrderDomainRepository orderDomainRepository;
+    private final OrderRepository orderRepository;
+
+    private final ProductRepository productRepository;
+
+    public List<OrderDto> getOrderList(String customerId) {
+
+        List<Order> orders = orderRepository.findOrders(customerId);
+
+        Map<String, List<ProductDto>> orderIdProductMap = orders.stream()
+                .collect(Collectors.groupingBy(
+                        Order::getOrderId,
+                        Collectors.mapping(
+                                order -> {
+                                    Product product = productRepository.findProduct(order.getProductId());
+                                    return ProductDto.builder()
+                                            .price(product.getPrice())
+                                            .name(product.getName())
+                                            .quantity(order.getQuantity())
+                                            .build();
+                                },
+                                Collectors.toList()
+                        )
+                ));
+
+        List<OrderDto> result = orderIdProductMap.entrySet().stream()
+                .map(entry ->
+                        OrderDto.builder()
+                                .orderId(entry.getKey())
+                                .products(entry.getValue())
+                                .createTime(orders.stream()
+                                        .filter(order -> entry.getKey().equals(order.getOrderId()))
+                                        .findFirst()
+                                        .map(Order::getCreateTime)
+                                        .orElse(null))
+                                .orderStatus(orders.stream()
+                                        .filter(order -> entry.getKey().equals(order.getOrderId()))
+                                        .findFirst()
+                                        .map(Order::getStatus)
+                                        .orElse(null))
+                                //todo
+                                .totalPrice(BigDecimal.valueOf(0))
+                                .build()
+                )
+                .collect(Collectors.toList());
+
+        return result;
+
+
+    }
 }
--- src/main/java/com/example/presentation/facade/OrderController.java
+++ src/main/java/com/example/presentation/facade/OrderController.java
@@ -1,5 +1,6 @@
 package com.example.presentation.facade;
 
+import com.example.application.service.OrderApplicationService;
 import com.example.presentation.vo.OrderDto;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/orders")
 public class OrderController {
-  @GetMapping("/{id}")
+
+  private final OrderApplicationService orderApplicationService;
+
+  @GetMapping("customer/{id}")
   public OrderDto getOrderDetails(@PathVariable String id) {
     return null;
     // Todo: need development
+  }
+
+  @GetMapping("/{customerId}")
+  public List<OrderDto> getOrderList(@PathVariable String customerId) {
+    return orderApplicationService.getOrderList(customerId);
   }
 }
