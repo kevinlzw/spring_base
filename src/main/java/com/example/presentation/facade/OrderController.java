3b06afa49f8556be4add97ef80635f93b3aecd1f
ChenZhuBuffalo <chen.zhu1@thoughtworks.com>
2023-08-08 14:33:24 +0800

    add getOrderDetails method in OrderController

--- src/main/java/com/example/presentation/facade/OrderController.java
+++ src/main/java/com/example/presentation/facade/OrderController.java
@@ -1,6 +1,10 @@
 package com.example.presentation.facade;
 
+import com.example.presentation.vo.OrderDto;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/orders")
 public class OrderController {
+    @GetMapping("/{id}")
+    public OrderDto getOrderDetails(@PathVariable String id) {
+        return null;
+        //Todo: need development
+    }
 }
