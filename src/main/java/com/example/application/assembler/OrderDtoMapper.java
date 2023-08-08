a61b45b7c98e498ac04a4967e4effb3c3191e0e3
ChenZhuBuffalo <chen.zhu1@thoughtworks.com>
2023-08-08 16:58:19 +0800

    modify OrderDtoMapper to interface

--- src/main/java/com/example/application/assembler/OrderDtoMapper.java
+++ src/main/java/com/example/application/assembler/OrderDtoMapper.java
@@ -1,9 +1,12 @@
 package com.example.application.assembler;
 
+import com.example.domain.entity.Order;
 import com.example.presentation.vo.OrderDto;
 
+import static org.mapstruct.factory.Mappers.getMapper;
+
 @org.mapstruct.Mapper(unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
-public class OrderDtoMapper {
+public interface OrderDtoMapper {
     OrderDtoMapper MAPPER = getMapper(OrderDtoMapper.class);
 
     OrderDto toDto(Order order);
