d54e93d492f895b271565e207880449d00a1fdbd
ChenZhuBuffalo <chen.zhu1@thoughtworks.com>
2023-08-09 16:20:06 +0800

    resolve conflicts

--- src/main/java/com/example/infrastructure/persistence/assembler/OrderDataMapper.java
+++ src/main/java/com/example/infrastructure/persistence/assembler/OrderDataMapper.java
@@ -1,16 +1,22 @@
 package com.example.infrastructure.persistence.assembler;
 
 import com.example.domain.entity.Order;
+import com.example.domain.entity.ProductDetail;
 import com.example.infrastructure.persistence.entity.OrderPo;
+import com.example.presentation.vo.ProductDetailsDto;
+import org.mapstruct.Mapping;
 import org.mapstruct.Mapper;
 
 import static org.mapstruct.factory.Mappers.getMapper;
 
-@Mapper(unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
+@org.mapstruct.Mapper(unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
 public interface OrderDataMapper {
   OrderDataMapper mapper = getMapper(OrderDataMapper.class);
 
   Order toDo(OrderPo orderPo);
 
   OrderPo toPo(Order order);
+  ProductDetailsDto toResponse(OrderPo orderPo);
+  @Mapping(source = "productId", target = "id")
+  ProductDetail toProductDetail(OrderPo orderPo);
 }
