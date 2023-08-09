16350d87f9ae805dc8b5c0eee73c9f89f549ba10
ChenZhuBuffalo <chen.zhu1@thoughtworks.com>
2023-08-09 16:25:21 +0800

    remove useless import

--- src/main/java/com/example/infrastructure/persistence/repository/domain/OrderDomainRepository.java
+++ src/main/java/com/example/infrastructure/persistence/repository/domain/OrderDomainRepository.java
@@ -1,12 +1,10 @@
 package com.example.infrastructure.persistence.repository.domain;
 
-import com.example.application.assembler.ProductDtoMapper;
 import com.example.domain.entity.Order;
 import com.example.domain.entity.ProductDetail;
 import com.example.domain.repository.OrderRepository;
 import com.example.infrastructure.persistence.assembler.OrderDataMapper;
 import com.example.infrastructure.persistence.repository.JpaOrderRepository;
-import com.example.presentation.vo.ProductDetailsDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
