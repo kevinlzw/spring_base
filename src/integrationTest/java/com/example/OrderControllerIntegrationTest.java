893b34702c5837fb08166451718081393b9afca4
shuang.gao <shuang.gao@thoughtworks.com>
2023-08-09 18:19:34 +0800

    Add OrderControllerIntegrationTest

--- src/integrationTest/java/com/example/OrderControllerIntegrationTest.java
+++ src/integrationTest/java/com/example/OrderControllerIntegrationTest.java
@@ -1,19 +1,31 @@
 package com.example;
 
+import com.example.presentation.vo.OrderDto;
 import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class OrderControllerIntegrationTest extends BaseIntegrationTest {
 
   @Test
   @DataSet("orders.yml")
-  public void findOrders_should_success() {
-    given().when().get("/orders").then().statusCode(200).body("[0].id", equalTo("12345"))
-        .body("[0].name", equalTo("idk")).body("[0].price.toString()", equalTo("223.34"))
-        .body("[0].status", equalTo("VALID"));
+  public void getOrderList_should_returnOrderList() {
+    String customerId = "12345";
+
+    List<OrderDto> orderList = given().param("customerId", customerId)
+            .when().get("/orders")
+            .then().statusCode(200)
+            .extract().body().jsonPath().getList(".", OrderDto.class);
+
+    assertThat(orderList).hasSize(2);
+    assertThat(orderList.get(0).getOrderId()).isEqualTo("ORD-123456");
+    assertThat(orderList.get(0).getTotalPrice()).isEqualTo(30.98);
+
   }
 }
+
